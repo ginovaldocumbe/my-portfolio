830341eef3a9304613cc0ca4a8b7bd1ad0e43946
ginovaldocumbe <92864360+ginovaldocumbe@users.noreply.github.com>
2025-06-23 16:49:08 +0200

    fix: Remove BrowserRouter wrapper from App component for simplified routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,13 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import routes from "@/routes/routes";
 
 function App() {
   return (
-    <BrowserRouter basename={import.meta.env.PROD ? '/my-portfolio' : '/'}>
-      <Routes>
-        {routes.map((route, index) => (
-          <Route key={index} path={route.path} element={route.element} />
-        ))}
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      {routes.map((route, index) => (
+        <Route key={index} path={route.path} element={route.element} />
+      ))}
+    </Routes>
   );
 }
 
