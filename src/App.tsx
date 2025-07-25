7a1d272addb4795a90980d5e7bcc182b58f332cc
ginovaldocumbe <92864360+ginovaldocumbe@users.noreply.github.com>
2025-07-25 12:36:09 +0200

    Replace meta refresh with JavaScript redirect and add session storage handling

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,14 @@ import { Routes, Route } from "react-router-dom";
 import routes from "@/routes/routes";
 
 function App() {
+  const redirectedUrl = sessionStorage.redirect;
+  if (redirectedUrl) {
+    sessionStorage.removeItem("redirect");
+    const url = new URL(redirectedUrl);
+    const pathname = url.pathname.replace("/my-portfolio", "");
+    window.history.replaceState(null, "", pathname);
+  }
+
   return (
     <Routes>
       {routes.map((route, index) => (
